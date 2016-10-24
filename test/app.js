249230f8c058f78c154d04f08fd04160b754f8b6
jean <jean.ma.1986@gmail.com>
2016-10-24 15:18:28 +0800

    add test

---
+++ test/app.js
@@ -0,0 +1,224 @@
+import React, {Component} from "react";
+import Router from "./router";
+import {View, Text, StyleSheet} from "react-native";
+
+const styles = StyleSheet.create({
+	container: {
+		marginTop: 60
+	},
+	button: {
+		backgroundColor: "black",
+		color: "white",
+		paddingTop: 10,
+		paddingBottom: 10,
+		fontSize: 12,
+		marginTop: 5,
+		marginBottom: 5,
+		textAlign: "center"
+	},
+	link: {
+		color: "blue",
+		textAlign: "center",
+		marginTop: 5,
+		marginBottom: 5
+	}
+});
+
+let isLogin = false;
+export function login() {
+	isLogin = true;
+}
+
+class Home extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			count: 0
+		};
+	}
+
+	render() {
+		return (
+			<View
+				style={styles.container}>
+				<Text style={styles.button} onPress={event=> {
+					this.props.navigator.$refreshNavBar({
+						hideNavigationBar: false
+					});
+				}}>show navigation bar</Text>
+				<Text style={styles.button} onPress={event=> {
+					this.props.navigator.$refreshNavBar({
+						hideNavigationBar: true
+					});
+				}}>hide navigation bar</Text>
+				<Text style={styles.button} onPress={event=> {
+					/*
+					this.props.navigator.$refreshNavBar({
+						title: "New Title"
+					});
+					*/
+					this.props.navigator.$refreshNavBar({
+						title:"New Title",
+						renderTitle:(route)=>{
+							return <Text style={{color:"red"}}>{route.title}</Text>
+						}
+					});
+				}}>set title</Text>
+				<Text style={styles.button} onPress={event=> {
+					this.props.navigator.$refreshNavBar();
+				}}>refresh</Text>
+				<Text style={styles.button} onPress={event=> {
+					this.props.navigator.$replace("register");
+				}}>replace</Text>
+				<Text style={styles.button} onPress={event=> {
+					this.setState({count: this.state.count + 1});
+				}}>{this.state.count}</Text>
+				<Text style={styles.link} onPress={event=> {
+					this.props.navigator.$push("register", {
+						renderRightButton: ()=> {
+							return <Text style={{color: "white"}}>ADD</Text>
+						}
+					});
+				}}>go register</Text>
+				<Text style={styles.link} onPress={event=> {
+					this.props.navigator.$push("mine");
+				}}>go account</Text>
+			</View>
+		);
+	}
+}
+
+class RegisterStep1 extends Component {
+
+	constructor(props) {
+		super(props);
+		console.log("init register step1");
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text
+					style={styles.link}
+					onPress={event=> {
+					this.props.navigator.$push("register/step2", {
+						message: "我是来自注册第一步的参数"
+					});
+				}}>go to register step2</Text>
+				<Text
+					style={styles.button}
+					onPress={event=> {
+					this.props.navigator.$refreshNavBar({
+						hideNavigationBar: true
+					});
+				}}>hide navigation bar</Text>
+			</View>
+		);
+	}
+}
+
+class RegisterStep2 extends Component {
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text>{this.props.route.message}</Text>
+				<Text
+					style={styles.button}
+					onPress={event=> {
+					this.props.navigator.$refreshNavBar({
+						hideNavigationBar: false
+					});
+				}}>show navigation bar</Text>
+				<Text
+					style={styles.button}
+					onPress={event=> {
+					this.props.navigator.$refreshNavBar({
+						renderRightButton: ()=> {
+							return <Text style={{color: "white"}}>REGISTER</Text>
+						}
+					});
+				}}>show right button</Text>
+				<Text
+					style={styles.button}
+					onPress={event=> {
+					this.props.navigator.$refreshNavBar({
+						renderRightButton: null
+					});
+				}}>hide right button</Text>
+			</View>
+		);
+	}
+}
+
+class Mine extends Component {
+	render() {
+		return <View style={styles.container}>
+			<Text>login success</Text>
+		</View>
+	}
+}
+
+class Login extends Component {
+	render() {
+		return <View style={styles.container}>
+			<Text
+				style={styles.button}
+				onPress={event=> {
+				isLogin = true;
+				this.props.navigator.$replace(this.props.route.$previousPath);
+			}}>login</Text>
+		</View>
+	}
+}
+
+const routes = [{
+	path: "home",
+	title: "Home",
+	component: Home
+}, {
+	path: "register",
+	title: "Register-Step1",
+	component: RegisterStep1,
+	routes: [{
+		path: "step2",
+		title: "Register-Step2",
+		component: RegisterStep2
+	}]
+}, {
+	path: "login",
+	title: "登录",
+	component: Login
+}, {
+	path: "mine",
+	title: "我的",
+	component: Mine,
+	onEnter: ()=> {
+		if (!isLogin) {
+			return "login";
+		}
+	}
+}];
+
+export default class APP extends Component {
+	render() {
+		return (
+			<Router ref="router" renderTitle={(route)=> {
+				return <Text style={{color: "white"}}>{route.title}</Text>;
+			}}
+					onChange={(type)=> {
+						console.log(type);
+					}}
+					renderLeftButton={(route, navigator, index)=> {
+						if (index > 0) {
+							return <Text style={{color: "white"}} onPress={event=> {
+								navigator.$pop();
+							}}>back</Text>
+						}
+						return null;
+					}}
+					navigationBarStyle={{backgroundColor: "black"}}
+					routes={routes}></Router>
+		);
+	}
+}
---
+++ test/router.js
@@ -0,0 +1,272 @@
+import React, {Component, PropTypes} from "react";
+import {Navigator} from "react-native";
+
+let {NavigationBar}=Navigator;
+
+/**
+ * NavigationBar extension
+ * @class
+ * @classdesc 增加了refresh方法,可以对navigation bar 进行更细粒度的操作
+ * */
+class NavigationBarEx extends NavigationBar {
+	/**
+	 * current route is readonly
+	 * */
+	get currentRoute() {
+		let routes = this.props.navState.routeStack;
+		if (routes.length && routes.length > 0) {
+			return Object.assign({}, routes[routes.length - 1]);
+		}
+		return null;
+	}
+
+	render() {
+		if (this.currentRoute && this.currentRoute.hideNavigationBar) {
+			return null;
+		}
+		return super.render();
+	}
+
+	/**
+	 * force refresh navigation bar with ops
+	 * @param {object} [ops={}] - the navigation bar ops
+	 * @param {string} [ops.title] - title
+	 * @param {function} [ops.renderLeftButton] - render left button or back button
+	 * @param {function} [ops.renderRightButton] - render right button
+	 * @param {function} [ops.renderTitle] - render title
+	 * @param {boolean} [ops.hideNavigationBar=false] - it is show navigation bar by ops.hideNavigationBar
+	 * */
+	refresh(ops = {}) {
+		let routes = this.props.navState.routeStack;
+		let route = routes[routes.length - 1];
+		routes[routes.length - 1] = Object.assign({}, route, ops);
+		this.forceUpdate();
+	}
+}
+
+/**
+ * Navigator extension
+ * @class
+ * */
+class NavigatorEx extends Navigator {
+	/**
+	 * current route , readonly
+	 * */
+	get currentRoute() {
+		let routes = this.state.routeStack;
+		if (routes.length && routes.length > 0) {
+			return Object.assign({}, routes[routes.length - 1]);
+		}
+		return null;
+	}
+
+	static propTypes = {
+		onChange: PropTypes.func
+	}
+
+	/**
+	 * find target route by path
+	 * @param {string} path - path like path1/path2
+	 * @returns {object} route
+	 * */
+	_findRouteByPath(path) {
+		let routes = this.props.routes;
+		let pathNames = path.split("/");
+		let route;
+		while (pathNames.length > 0) {
+			let pathName = pathNames.shift();
+			if (route) {
+				if (route.routes) {
+					route = route.routes.find(r=>r.path === pathName);
+				}
+				else {
+					throw new Error(`${route.path} not defined routes`);
+				}
+			}
+			else {
+				route = this.props.routes.find(r=>r.path === pathName);
+			}
+		}
+		if (!route) {
+			throw new Error(`route not found , path = ${path}`);
+		}
+		return route;
+	}
+
+	/**
+	 * push route to router stack
+	 * @param {string} path - path
+	 * @param {object} ops - ops apply the route which find by path
+	 * @param {string} [ops.title] - title
+	 * @param {function} [ops.renderLeftButton] - render left button or back button
+	 * @param {function} [ops.renderRightButton] - render right button
+	 * @param {function} [ops.renderTitle] - render title
+	 * @param {boolean} [ops.hideNavigationBar=false] - it is show navigation bar by ops.hideNavigationBar
+	 * @param {function} [ops.onEnter]
+	 * */
+	$push(path, ops = {}) {
+		let route = {
+			...this._findRouteByPath(path),
+			...ops
+		};
+		if (route.onEnter && typeof route.onEnter === "function") {
+			let redirectPath = route.onEnter(route);
+			if (redirectPath) {
+				route = {
+					...this._findRouteByPath(redirectPath),
+					...ops,
+					$previousPath: path,
+					$previousRoute: route
+				}
+			}
+		}
+		this.props.onChange("$push", route, path);
+		this.push(route);
+	}
+
+	/**
+	 * pop route from router stack
+	 * */
+	$pop() {
+		this.props.onChange("$pop", this.currentRoute);
+		this.pop();
+	}
+
+	/**
+	 * replace current route with the path
+	 * @param {string} path - path
+	 * @param {object} ops - ops apply the route which find by path
+	 * @param {string} [ops.title] - title
+	 * @param {function} [ops.renderLeftButton] - render left button or back button
+	 * @param {function} [ops.renderRightButton] - render right button
+	 * @param {function} [ops.renderTitle] - render title
+	 * @param {boolean} [ops.hideNavigationBar=false] - it is show navigation bar by ops.hideNavigationBar
+	 * @param {function} [ops.onEnter]
+	 * */
+	$replace(path, ops = {}) {
+		let route = {
+			...this._findRouteByPath(path),
+			...ops
+		};
+		this.props.onChange("$replace", route, path);
+		this.replace(route);
+	}
+
+	/**
+	 * refresh navigation bar with ops
+	 * @param {object} [ops={}] - the navigation bar ops
+	 * @param {string} [ops.title] - title
+	 * @param {function} [ops.renderLeftButton] - render left button or back button
+	 * @param {function} [ops.renderRightButton] - render right button
+	 * @param {function} [ops.renderTitle] - render title
+	 * @param {boolean} [ops.hideNavigationBar=false] - it is show navigation bar by ops.hideNavigationBar
+	 * */
+	$refreshNavBar(ops = {}) {
+		setTimeout(()=> {
+			this.props.onChange("$refreshNavBar", ops);
+			this._navBar.refresh(ops);
+		}, 128);
+	}
+
+}
+
+/**
+ * Router
+ * @class
+ * */
+export default class Router extends Component {
+	constructor(props) {
+		super(props);
+		this.initialRoute = props.routes[0];
+		this.navigationBarEx = <NavigationBarEx ref="navigationBar" {...this._buildNavigationBar()}/>;
+	}
+
+	static propTypes = {
+		renderLeftButton: PropTypes.func.isRequired,
+		renderTitle: PropTypes.func,
+		routes: PropTypes.array.isRequired,
+		navigationBarStyle: PropTypes.any,
+		configureScene: PropTypes.func,
+		onChange: PropTypes.func
+	}
+
+	static defaultProps = {
+		navigationBarStyle: {},
+		configureScene: ()=> {
+			return Navigator.SceneConfigs.HorizontalSwipeJump;
+		},
+		onChange: ()=> {
+		}
+	};
+
+	_buildNavigationBar() {
+		let defaultRenderLeftButton = this.props.renderLeftButton;
+		let defaultRenderTitle = this.props.renderTitle;
+		let defaultNavigationBarStyle = this.props.navigationBarStyle;
+		let navigationBarProps = {
+			routeMapper: {
+				LeftButton: (route, navigator, index, navState) => {
+					if (route.renderLeftButton) {
+						return route.renderLeftButton(route, navigator, index, navState);
+					}
+					if (defaultRenderLeftButton) {
+						return defaultRenderLeftButton(route, navigator, index, navState);
+					}
+					console.warn("Router.renderLeftButton is missed.");
+				},
+				RightButton: (route, navigator, index, navState) => {
+					if (route.renderRightButton) {
+						return route.renderRightButton(route, navigator, index, navState);
+					}
+					return null;
+				},
+				Title: (route, navigator, index, navState) => {
+					if (route.renderTitle) {
+						return route.renderTitle(route, navigator, index, navState);
+					}
+					if (defaultRenderTitle) {
+						return defaultRenderTitle(route, navigator, index, navState);
+					}
+					return <Text style={{color: "white"}}>{route.title}</Text>;
+				}
+			},
+			style: defaultNavigationBarStyle
+		};
+		return navigationBarProps;
+	}
+
+	render() {
+		return (
+			<NavigatorEx
+				initialRoute={this.initialRoute}
+				onDidFocus={route=> {
+					if (route.$ref) {
+						if (route.$ref.sceneDidFocus) {
+							route.$ref.sceneDidFocus(route);
+						}
+						else if (route.$ref.renderedElement
+							&& route.$ref.renderedElement._owner
+							&& route.$ref.renderedElement._owner._renderedComponent
+							&& route.$ref.renderedElement._owner._renderedComponent._instance
+							&& route.$ref.renderedElement._owner._renderedComponent._instance.sceneDidFocus) {
+							route.$ref.renderedElement._owner._renderedComponent._instance.sceneDidFocus(route);
+						}
+					}
+				}}
+				routes={this.props.routes}
+				onChange={this.props.onChange.bind(this)}
+				navigationBar={this.navigationBarEx}
+				configureScene={(route, routeStack)=> {
+					return this.props.configureScene(route, routeStack);
+				}}
+				renderScene={(route, navigator)=> {
+					return <route.component
+						ref={ref=> {
+							route.$ref = ref;
+						}}
+						navigator={navigator}
+						route={route}/>
+				}}></NavigatorEx>
+		);
+	}
+}
